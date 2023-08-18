268f983b7ca15a08f2ef0e96da64e341b1cc1ad4
ingandresmolero <87983934+ingandresmolero@users.noreply.github.com>
2023-08-18 09:52:06 -0400

    Update crearusuario.php

--- views/operacion/crearusuario.php
+++ views/operacion/crearusuario.php
@@ -6,13 +6,29 @@
         $usuario = $_POST['usuario'];
         $contrasena = $_POST['clave'];
         $rol = $_POST['rol'];
+        
 
-        $sql = "INSERT INTO tbladmin (AdminName, UserName, Password, Role) VALUES ('$nombre','$usuario','$contrasena','$rol')";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
+        if($rol != 'usuario'){
+            $sql = "INSERT INTO tbladmin (AdminName, UserName, Password, Role) VALUES ('$nombre','$usuario','$contrasena','$rol')";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+    
+          
+               header('Location: ../usuarios.php');
+        }else if($rol =='usuario') {
+            $sql = "INSERT INTO tbladmin (AdminName, UserName, Password, Role) VALUES ('$nombre','$usuario','$contrasena','$rol')";
+            $sqlservidor ="INSERT INTO tblbarber( nombre, porcentaje, usuario, pass, Codigo, Puesto, Telefono) VALUES ('$nombre','0','$usuario','$contrasena','0','0','0')";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
 
-      
-           header('Location: ../usuarios.php');
+            $stmt2 = $conn->prepare($sqlservidor);
+            $stmt2->execute();
+    
+          
+               header('Location: ../usuarios.php');
+        }
+
+       
         
     }
 
